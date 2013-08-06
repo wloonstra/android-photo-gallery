c1b6c3a5c7c4757a1ded8705784217b2bc976f28
Wiggert <wloonstra@ebay.com>
2013-08-06 12:01:40 +0200

    509 Open a webpage with title and progress bar

--- src/com/bignerdranch/android/photogallery/PhotoPageFragment.java
+++ src/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PhotoPageFragment extends VisibleFragment {
 
@@ -26,6 +29,10 @@ public class PhotoPageFragment extends VisibleFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
+        final ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.setMax(100); // WebChromeClient reports in range 0-100
+        final TextView titleTextView = (TextView) v.findViewById(R.id.titleTextView);
+
         mWebView = (WebView) v.findViewById(R.id.webView);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -34,6 +41,23 @@ public class PhotoPageFragment extends VisibleFragment {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                titleTextView.setText(title);
             }
         });
 
